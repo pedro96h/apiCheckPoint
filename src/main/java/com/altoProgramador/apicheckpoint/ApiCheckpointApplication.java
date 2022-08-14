50a3c5f2630868200c251174823af2b56bc5b836
Pedro Nascimento <pedro.nasncimento@maida.health>
2022-08-14 19:30:59 -0300

    starting the project

---
+++ src/main/java/com/altoProgramador/apicheckpoint/ApiCheckpointApplication.java
@@ -0,0 +1,13 @@
+package com.altoProgramador.apicheckpoint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiCheckpointApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiCheckpointApplication.class, args);
+	}
+
+}
